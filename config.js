be593ce045bdd92c096d79553360f7b0e49e2e14
ThinNguyen100420 <88636953+ThinNguyen100420@users.noreply.github.com>
2021-08-10 10:17:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Yêu anh nhiều nhiềuuu <33',
+    btnNo: 'Yêu anh ít à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tại vì em yêu anh! Thế thôi <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Công chúa ngủ ngon nhaaaa <3333.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/yeucongchuaBichTien/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
